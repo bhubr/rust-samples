9645acd729b62a93f14ef554e80a69673ddbe025
Benoît Hubert <benoithubert@gmail.com>
2021-06-21 14:39:15 +0200

    Create a vector of s

--- address-book/address-book.rs
+++ address-book/address-book.rs
@@ -1,46 +1,53 @@
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::prelude::*;
 use std::io::Write;
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::path::Path;
 
 fn create_file_if_not_exists(file_name: &str) -> std::io::Result<()> {
-
-  let file_path = Path::new(file_name);
-  // https://doc.rust-lang.org/std/path/struct.Path.html#method.exists
-  let file_exists = file_path.exists();
-  if !file_exists {
-    println!("File does not exists");
-    // https://doc.rust-lang.org/rust-by-example/std_misc/file/create.html
-    let mut file = File::create(&file_name)?;
-    file.write(b"name,email,phone")?;
-  };
-  Ok(())
+    let file_path = Path::new(file_name);
+    // https://doc.rust-lang.org/std/path/struct.Path.html#method.exists
+    let file_exists = file_path.exists();
+    if !file_exists {
+        println!("File does not exists");
+        // https://doc.rust-lang.org/rust-by-example/std_misc/file/create.html
+        let mut file = File::create(&file_name)?;
+        file.write(b"name,email,phone")?;
+    };
+    Ok(())
 }
 
-fn main()-> std::io::Result<()> {
-  let file_name = "address-book.csv";
-  create_file_if_not_exists(&file_name);
+struct Contact {
+    name: String,
+    email: String,
+    phone: String,
+}
 
-  let mut v: Vec<_> = Vec::new();
-  if let Ok(lines) = read_lines(file_name) {
-    // Consumes the iterator, returns an (Optional) String
-    let mut i = 0;
-    for line in lines {
-        if i > 0 {
-            let d: Vec<_> = Ok(line).split(',').collect();
-            v.push(d);
+fn main() -> std::io::Result<()> {
+    let file_name = "address-book.csv";
+    create_file_if_not_exists(&file_name);
+
+    // https://doc.rust-lang.org/std/fs/struct.File.html#examples
+    // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+    // https://rust-lang-nursery.github.io/rust-cookbook/file/read-write.html#read-lines-of-strings-from-a-file
+    let file = File::open(&file_name)?;
+    let buf_reader = BufReader::new(file);
+    // let mut contents = String::new();
+    // buf_reader.read_to_string(&mut contents)?;
+    let mut v: Vec<Contact> = Vec::new();
+    for (num, line) in buf_reader.lines().enumerate() {
+        // TODO check if line 0 contains name,email,phone
+        if num > 0 {
+            let line = line?.clone();
+            let bits: Vec<&str> = line.split(',').collect();
+            println!("{:?}", bits);
+            let c: Contact = Contact {
+                name: bits[0].to_string(),
+                email: bits[1].to_string(),
+                phone: bits[2].to_string(),
+            };
+            v.push(c);
         }
-        i += 1;
     }
-  }
-
-  Ok(())
+    Ok(())
 }
